eaf76e9e9e8a09aedbfb3e3b5ddbb0e8751a313e
Davide Uberti <ubertidavide@gmail.com>
2018-03-17 13:17:44 +0100

    added reverse hibernate config

--- ChatCom/src/java/chatcom/hibernateutil/HibernateUtil.java
+++ ChatCom/src/java/chatcom/hibernateutil/HibernateUtil.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package chatcom.model;
+package chatcom.hibernateutil;
 
 import org.hibernate.cfg.AnnotationConfiguration;
 import org.hibernate.SessionFactory;
