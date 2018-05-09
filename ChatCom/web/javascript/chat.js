92b797882e8169ae43d94d230e777505089f0069
Omar Facchini <User@DESKTOP-ICABBBI.homenet.telecomitalia.it>
2018-05-10 01:33:15 +0200

    fixed APIs, now shows the chats and the messages in the chat

--- ChatCom/web/javascript/chat.js
+++ ChatCom/web/javascript/chat.js
@@ -8,6 +8,7 @@
 
 //var rootURL = "https://jsonplaceholder.typicode.com/posts";
 var rootURL = "http://localhost:8080/ChatCom/api/user";
+var rootURLMessage = "http://localhost:8080/ChatCom/api/message";
 
 
 
@@ -29,72 +30,71 @@ $("#buttonopen").click(function () {
 
 //checkInstance(${user.id});
 
+window.setInterval(function(){
+  $('.profilecard').remove();
+  $('.messaggio').remove();
+  getChat();
+  getAllMessages();
+}, 5000);
 
-function checkInstance(user) {
-    $.ajax({
-        url: "http://localhost:8080/api/instance?userid= " + user,
-        type: "GET",
 
-        contentType: 'application/json; charset=utf-8',
-        success: function (data) {
 
-        }
-    })
-}
-
-getAllMessages();
-
-function getAllMessages() {
+function getChat() {
     $.ajax({
         url: rootURL,
         type: "GET",
 
         contentType: 'application/json; charset=utf-8',
-//            success: function(data) {
+
+        success: function (data) {
+            $.each(data, function (index, data) {
+                /*if (data.id % 2 == 0)  //invece di utilizzare l'id fare tipo data.username == sessione.username
+                    dxtext(data.nickname, data.lastname);
+                else
+                    sxtext(data.nickname, data.lastname);*/
+                addchat(data.nickname, data.lastname);
+            });
+        }
+//                            success: function(data) {
 //                        $.each(data, function (index, data){
-//                           if(data.id % 2 == 0)  //invece di utilizzare l'id fare tipo data.username == sessione.username
-//                               dxtext(data.title,data.body);
-//                           else
-//                               sxtext(data.title,data.body);
-//                           addchat(data.title,data.body);
+//                           $("#chat"+index).click(function() {
+//                               dxtext(data.data, data.data);
+//                            });
 //                        });
-//            }
-//        success: function (data) {
-//            $.each(data, function (index, data) {
-//                if (data.id % 2 == 0)  //invece di utilizzare l'id fare tipo data.username == sessione.username
-//                    dxtext(data.nickname, data.lastname);
-//                else
-//                    sxtext(data.nickname, data.lastname);
-//                addchat(data.nickname, data.lastname);
-//            });
-//        }
-                            success: function(data) {
-                        $.each(data, function (index, data){
-                            addchat(data.nickname,data.lastname,index);
-                           $("#chat"+index).click(function() {
-                                alert(data.nickname);
-                            });
-                        });
-                    }
+//                    }
     });
 }
 
-
-//pensavo di aggiungere qua e sul dxtext la stampa del message o fare un getMessage che creera una string
-// da passare come parametro qua
 function sxtext(name, text) {
-    $('#start').append('<div class="row"><div class="col-3"></div><div class="col-2"><div class="card message"><div class="card-header"><h6 class="card-title">' + name + '</h6></div><div class="card-body"><p class="card-text">' + text + '</p></div></div></div><div class="col-7"></div></div>');
+    $('#start').append('<div class="row messaggio"><div class="col-3"></div><div class="col-2"><div class="card message"><div class="card-header"><h6 class="card-title">' + name + '</h6></div><div class="card-body"><p class="card-text">' + text + '</p></div></div></div><div class="col-7"></div></div>');
 }
 
 function dxtext(name, text) {
-    $('#start').append('<div class="row"><div class="col-7"></div><div class="col-2"><div class="card message"><div class="card-header"><h6 class="card-title">' + name + '</h6></div><div class="card-body"><p class="card-text">' + text + '</p></div></div></div><div class="col-3"></div></div>');
+    $('#start').append('<div class="row messaggio"><div class="col-7"></div><div class="col-2"><div class="card message"><div class="card-header"><h6 class="card-title">' + name + '</h6></div><div class="card-body"><p class="card-text">' + text + '</p></div></div></div><div class="col-3"></div></div>');
 }
 
 function addchat(name, text, id) {
     $('#secondstart').append('<div id="chat'+id+'" class="card profilecard"><div class="card-header"><img class="profileimage" src="images/test.jpeg"></div><div class="card-body"><h5 class="card-title">' + name + '</h5><h6 class="card-subtitle mb-2 text-muted">' + text + '</h6></div></div>');
 }
 
-
-
+function getAllMessages(){
+    $.ajax({
+        url: rootURLMessage,
+        type: "GET",
+        
+        contentType: 'application/json; charset=utf-8',
+        
+        success: function (data){
+            $.each(data, function(index,data){
+                if(data.id % 2 == 0)
+                    dxtext("test", data.data);
+                else
+                    sxtext("test", data.data);
+                    //addchat(data.nickname, data.lastname, index);
+            });  
+        }
+    }); 
+    
+}
 
 
